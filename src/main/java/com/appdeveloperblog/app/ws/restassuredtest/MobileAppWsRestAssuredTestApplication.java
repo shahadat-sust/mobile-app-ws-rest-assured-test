9b1308cb166ff6f5d8651d716045a3e26abf1f52
Shahadat Hossain <shahadat.hossain@gmail.com>
2019-08-19 15:27:24 +0600

    First time commit

---
+++ src/main/java/com/appdeveloperblog/app/ws/restassuredtest/MobileAppWsRestAssuredTestApplication.java
@@ -0,0 +1,13 @@
+package com.appdeveloperblog.app.ws.restassuredtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileAppWsRestAssuredTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileAppWsRestAssuredTestApplication.class, args);
+	}
+
+}
